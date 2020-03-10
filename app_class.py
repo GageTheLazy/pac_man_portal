c2678e8362a000b43f1cbf40fa679d17ebb8a350
GageTheLazy <60364461+GageTheLazy@users.noreply.github.com>
2020-03-10 14:20:58 -0700

    Add files via upload

---
+++ app_class.py
@@ -0,0 +1,248 @@
+import sys
+from player import *
+from ghosts import *
+
+pygame.mixer.init()
+intro_music = pygame.mixer.Sound('pacman_beginning.wav')
+
+pygame.init()
+vec = pygame.math.Vector2
+pygame.display.set_caption("Pac-Man Portal by Michael Dimapindan")
+
+intro_music.play()
+
+
+class App:
+    def __init__(self):
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.state = 'start'
+        self.cell_width = MAZE_WIDTH//COLS
+        self.cell_height = MAZE_HEIGHT//ROWS
+        self.walls = []
+        self.coins = []
+        self.enemies = []
+        self.e_pos = []
+        self.p_pos = None
+        self.load()
+        self.pacman = Player(self, vec(self.p_pos))
+        self.make_enemies()
+
+    def run(self):
+        while self.running:
+            if self.state == 'start':
+                self.start_events()
+                self.start_update()
+                self.start_draw()
+            elif self.state == 'playing':
+                self.playing_events()
+                self.playing_update()
+                self.playing_draw()
+            elif self.state == 'game over':
+                self.game_over_events()
+                self.game_over_update()
+                self.game_over_draw()
+            elif self.state == 'winner':
+                self.you_win_events()
+                self.you_win_update()
+                self.you_win_draw()
+            else:
+                self.running = False
+            self.clock.tick(FPS)
+        pygame.quit()
+        sys.exit()
+
+# -------------------------- Helper Functions ---------------------------
+
+    def draw_text(self, words, screen, pos, size, colour, font_name, centered=False):
+        font = pygame.font.SysFont(font_name, size)
+        text = font.render(words, False, colour)
+        text_size = text.get_size()
+        if centered:
+            pos[0] = pos[0]-text_size[0]//2
+            pos[1] = pos[1]-text_size[1]//2
+        screen.blit(text, pos)
+
+    def load(self):
+        self.background = pygame.image.load('pac_maze.png')
+        self.background = pygame.transform.scale(self.background, (MAZE_WIDTH, MAZE_HEIGHT))
+
+        with open("walls.txt", 'r') as file:
+            for yidx, line in enumerate(file):
+                for xidx, char in enumerate(line):
+                    if char == "1":
+                        self.walls.append(vec(xidx, yidx))
+                    elif char == "C":
+                        self.coins.append(vec(xidx, yidx))
+                    elif char == "P":
+                        self.p_pos = [xidx, yidx]
+                    elif char in ["2", "3", "4", "5"]:
+                        self.e_pos.append([xidx, yidx])
+                    elif char == "B":
+                        pygame.draw.rect(self.background, BLACK, (xidx*self.cell_width, yidx*self.cell_height,
+                                                                  self.cell_width, self.cell_height))
+
+    def make_enemies(self):
+        for idx, pos in enumerate(self.e_pos):
+            self.enemies.append(Enemy(self, vec(pos), idx))
+
+    def draw_grid(self):
+        for x in range(WIDTH//self.cell_width):
+            pygame.draw.line(self.background, GRAY, (x*self.cell_width, 0),
+                             (x*self.cell_width, HEIGHT))
+        for x in range(HEIGHT//self.cell_height):
+            pygame.draw.line(self.background, GRAY, (0, x*self.cell_height),
+                             (WIDTH, x*self.cell_height))
+
+    def reset(self):
+        self.pacman.lives = 3
+        self.pacman.current_score = 0
+        self.pacman.grid_pos = vec(self.pacman.starting_pos)
+        self.pacman.pix_pos = self.pacman.get_pix_pos()
+        self.pacman.direction *= 0
+        for enemy in self.enemies:
+            enemy.grid_pos = vec(enemy.starting_pos)
+            enemy.pix_pos = enemy.get_pix_pos()
+            enemy.direction *= 0
+
+        self.coins = []
+        with open("walls.txt", 'r') as file:
+            for yidx, line in enumerate(file):
+                for xidx, char in enumerate(line):
+                    if char == 'C':
+                        self.coins.append(vec(xidx, yidx))
+        self.state = "playing"
+
+
+# --------------------------- Intro Functions --------------------------
+
+    def start_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                self.state = 'playing'
+
+    def start_update(self):
+        pass
+
+    def start_draw(self):
+        self.screen.fill(BLACK)
+        self.draw_text('Pac-Man Portal', self.screen, [
+                       WIDTH//2, HEIGHT//2-50], 40, ORANGE, START_FONT, centered=True)
+        self.draw_text('Press Space to Play', self.screen, [
+                       WIDTH//2, HEIGHT//2+50], 40, L_BLUE, START_FONT, centered=True)
+        pygame.display.update()
+
+# ----------------------- Playing Functions -------------------------
+
+    def playing_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self.pacman.move(vec(-1, 0))
+                if event.key == pygame.K_RIGHT:
+                    self.pacman.move(vec(1, 0))
+                if event.key == pygame.K_UP:
+                    self.pacman.move(vec(0, -1))
+                if event.key == pygame.K_DOWN:
+                    self.pacman.move(vec(0, 1))
+
+    def playing_update(self):
+        self.pacman.update()
+        for enemy in self.enemies:
+            enemy.update()
+
+        for enemy in self.enemies:
+            if enemy.grid_pos == self.pacman.grid_pos:
+                self.remove_life()
+        self.win_maze()
+
+    def playing_draw(self):
+        self.screen.fill(BLACK)
+        self.screen.blit(self.background, (TOP_BOTTOM_BUFFER//2, TOP_BOTTOM_BUFFER//2))
+        self.draw_coins()
+        self.draw_text('SCORE: {}'.format(self.pacman.current_score),
+                       self.screen, [60, 0], 18, WHITE, START_FONT)
+        self.pacman.draw()
+        for enemy in self.enemies:
+            enemy.draw()
+        pygame.display.update()
+
+    def remove_life(self):
+        self.pacman.lives -= 1
+        death_sound.play()
+        if self.pacman.lives == 0:
+            self.state = "game over"
+        else:
+            self.pacman.grid_pos = vec(self.pacman.starting_pos)
+            self.pacman.pix_pos = self.pacman.get_pix_pos()
+            self.pacman.direction *= 0
+            for enemy in self.enemies:
+                enemy.grid_pos = vec(enemy.starting_pos)
+                enemy.pix_pos = enemy.get_pix_pos()
+                enemy.direction *= 0
+
+    def win_maze(self):
+        if self.pacman.current_score == 28700:
+            self.state = "winner"
+            victory.play()
+
+    def draw_coins(self):
+        for coin in self.coins:
+            pygame.draw.circle(self.screen, (124, 123, 7),
+                               (int(coin.x*self.cell_width)+self.cell_width//2+TOP_BOTTOM_BUFFER//2,
+                                int(coin.y*self.cell_height)+self.cell_height//2+TOP_BOTTOM_BUFFER//2), 5)
+
+# ------------------------------- Game Over Functions -----------------------------
+
+    def game_over_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                self.reset()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                self.running = False
+
+    def game_over_update(self):
+        pass
+
+    def game_over_draw(self):
+        self.screen.fill(BLACK)
+        quit_text = "Press the escape button to QUIT"
+        again_text = "Press SPACE bar to PLAY AGAIN"
+        self.draw_text("GAME OVER", self.screen, [WIDTH//2, 100],  52, RED, "arial", centered=True)
+        self.draw_text(again_text, self.screen, [
+                       WIDTH//2, HEIGHT//2],  36, (190, 190, 190), "arial", centered=True)
+        self.draw_text(quit_text, self.screen, [
+                       WIDTH//2, HEIGHT//1.5],  36, (190, 190, 190), "arial", centered=True)
+        pygame.display.update()
+
+# ------------------------------ Winning Game Functions ----------------------------
+
+    def you_win_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                self.reset()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                self.running = False
+
+    def you_win_update(self):
+        pass
+
+    def you_win_draw(self):
+        self.screen.fill(BLACK)
+        win_text = "Press ESC to exit the game"
+        repeat_text = "Press SPACE to play again"
+        self.draw_text("YOU WIN!", self.screen, [WIDTH // 2, 100], 52, L_BLUE, "arial", centered=True)
+        self.draw_text(repeat_text, self.screen, [
+            WIDTH // 2, HEIGHT // 2], 36, (190, 190, 190), "arial", centered=True)
+        self.draw_text(win_text, self.screen, [
+            WIDTH // 2, HEIGHT // 1.5], 36, (190, 190, 190), "arial", centered=True)
+        pygame.display.update()
---
+++ main.py
@@ -0,0 +1,7 @@
+# Michael Gage Dimapindan, CPSC 386, 11:30AM-12:45PM
+from app_class import *
+
+
+if __name__ == '__main__':
+    app = App()
+    app.run()
